bf6e858363f0f1e359056024a594067e2cacc159
Sameer <akita.sameer1@gmail.com>
2025-10-19 23:28:43 +0900

    SetupRoutes method

--- main.go
+++ main.go
@@ -1,2 +1,35 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+type Repository struct{
+	DB *gorm.DB
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_books", r.CreateBook)
+	api.Delete("delete_book/:id", r.DeleteBook)
+	api.Get("/get_books/:id", r.GetBookByID)
+	api.Get("/books", r.GetBooks)
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := Repository{
+		DB: db,
+	}
+
+	app := fiber.New()
+	r.SetupRoutes(app)
+	app.Listen(":8080")
+}
